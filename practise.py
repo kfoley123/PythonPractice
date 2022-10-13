0d2fd623498a50e5e89e0e9df1eecc55060563f4
Kortney <kortney@iMac.local>
2022-10-13 15:55:34 -0300

    did some practise questions and ran them

--- practise.py
+++ practise.py
@@ -10,3 +10,34 @@ def front3(str):
   word = str[0:3]
   
   return word + word + word 
+
+print(front3("dog"))
+
+print("==========================================")
+
+  
+#Given a string, return a new string where the first and last chars have been exchanged.
+
+
+#front_back('code') → 'eodc'
+#front_back('a') → 'a'
+#front_back('ab') → 'ba'
+
+def front_back(str):
+  if len(str) <= 1:
+    return str
+    
+  mid = str[1:-1]
+  
+  return str[len(str)-1] + mid + str[0]
+
+print(front_back("hello"))
+
+
+
+#Given a non-empty string and an int n, return a new string where the char at index n has been removed. The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..len(str)-1 inclusive).
+
+
+#missing_char('kitten', 1) → 'ktten'
+#missing_char('kitten', 0) → 'itten'
+#missing_char('kitten', 4) → 'kittn'
