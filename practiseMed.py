3e5a95a8d12480d1f56971ebbaf82fa924ec4f96
Kortney <kortney@iMac.local>
2022-10-14 14:50:15 -0300

    did question 1 stringTimes

---
+++ practiseMed.py
@@ -0,0 +1,18 @@
+# med practise question with loops 
+
+print("=====================================")
+
+#Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+
+#string_times('Hi', 2) → 'HiHi'
+#string_times('Hi', 3) → 'HiHiHi'
+#string_times('Hi', 1) → 'Hi'
+
+def string_times(str, n):
+  result = ""
+  for _ in range(n):
+   result += str
+  return result 
+
+
