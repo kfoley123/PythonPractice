99e1a26c4edf80ff170b14bc5ae232b01f3b60e8
Kortney <kortney@iMac.local>
2022-10-14 14:06:46 -0300

    did sleepIn practise question

--- practise.py
+++ practise.py
@@ -194,3 +194,18 @@ def monkey_trouble(a_smile, b_smile):
 print(monkey_trouble(True, False))
 
 print("==========================================")
+
+#The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
+
+
+#sleep_in(False, False) → True
+#sleep_in(True, False) → False
+#sleep_in(False, True) → True
+
+def sleep_in(weekday, vacation):
+  if weekday == False or vacation == True:
+    return True
+  else:
+    return False 
+
+print(sleep_in(False, False))
