d9731a148f9a58e924547e4035b9f76dd9ee99b8
Kortney <kortney@iMac.local>
2022-10-14 12:08:22 -0300

    did makes10 practise question

--- practise.py
+++ practise.py
@@ -110,3 +110,20 @@ def near_hundred(n):
 print(near_hundred(102))
 
 print("==========================================")
+
+#Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
+
+
+#makes10(9, 10) → True
+#makes10(9, 9) → False
+#makes10(1, 9) → True
+
+def makes10(a, b):
+  if a == 10 or b == 10:
+    return True 
+  if a + b == 10:
+    return True 
+  else:
+    return False 
+
+print(makes10(5,5))
