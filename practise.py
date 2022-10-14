d8621d23e19b8273b5005d2e24b56cbd8e4c7242
Kortney <kortney@iMac.local>
2022-10-14 13:46:32 -0300

    did absolute difference n & 21 practise question

--- practise.py
+++ practise.py
@@ -143,3 +143,21 @@ def parrot_trouble(talking, hour):
 print(parrot_trouble(True, 9))
 
 print("==========================================")
+
+#Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+
+
+#diff21(19) → 2
+#diff21(10) → 11
+#diff21(21) → 0
+
+def diff21(n):
+  if n > 21:
+    return abs(21-n)*2
+  else:
+    return abs(21-n)
+
+print(diff21(3))
+
+print("==========================================")
+
