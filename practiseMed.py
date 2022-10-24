718aa2c615ae90587bdc629809bade6bfacf9f52
Kortney <kortney@iMac.local>
2022-10-24 15:20:39 -0300

    did practise question 8

--- practiseMed.py
+++ practiseMed.py
@@ -122,3 +122,8 @@ def array_front9(nums):
 # array123([1, 1, 2, 4, 1]) → False
 # array123([1, 1, 2, 1, 2, 3]) → True
 
+def array123(nums):
+  for i in range(len(nums) -2):
+    if nums[i]==1 and nums[i+1]==2 and nums[i+2]==3:
+      return True
+  return False 
