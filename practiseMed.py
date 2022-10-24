accbd1806d2c5f04aba40dcbb0d19a8bb8f31444
Kortney <kortney@iMac.local>
2022-10-24 13:54:38 -0300

    did practice question 7 array_front9

--- practiseMed.py
+++ practiseMed.py
@@ -74,7 +74,12 @@ print(string_splosion("meow"))
 
 print("=====================================")
 
-#5 
+#5 Given a string, return the count of the number of times that a substring length 2 appears in the string and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+
+
+# last2('hixxhi') → 1
+# last2('xaxxaxaxx') → 1
+# last2('axxxaaxx') → 2
 
 #6 Given an array of ints, return the number of 9's in the array.
 
@@ -91,3 +96,21 @@ print(array_count9([1, 9, 9, 5]))
 
 print("=====================================")
 
+#7 Given an array of ints, return True if one of the first 4 elements in the array is a 9. The array length may be less than 4.
+
+
+# array_front9([1, 2, 9, 3, 4]) → True
+# array_front9([1, 2, 3, 4, 9]) → False
+# array_front9([1, 2, 3, 4, 5]) → False
+
+def array_front9(nums):
+  for count, value in enumerate(nums):
+    
+    if count <= 3 and value == 9:
+      return True
+   
+  return False 
+
+  print(array_front9([1,2,7,5,9]))
+
+  print("=====================================")
